ddd9a96281353c8973dda0b091e41e39756608ff
fabianhinz <hinzfabian.fh@gmail.com>
2020-05-16 17:14:15 +0200

    cleanup

--- sb/src/index.js
+++ sb/src/index.js
@@ -1,26 +1,6 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
-import './style.css';
+import React from 'react';
+import {render} from 'react-dom';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'React'
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => <div>hello</div>
 
 render(<App />, document.getElementById('root'));
