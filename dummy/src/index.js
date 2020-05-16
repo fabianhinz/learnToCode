6fda60ed147ad5804c65074992dad013ec21ad97
fabianhinz <hinzfabian.fh@gmail.com>
2020-05-16 17:25:33 +0200

    mui dep

--- dummy/src/index.js
+++ dummy/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {render} from 'react-dom';
+import {render} from 'react-dom'
+import Button from '@material-ui/core/Button'
 
-const App = () => <div>hello</div>
+const App = () => <Button variant="contained" color="default">click me </Button>
 
 render(<App />, document.getElementById('root'));
