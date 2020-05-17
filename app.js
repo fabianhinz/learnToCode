05f29e5ec879e996b8f4cfbd38280a72d00c78b9
fabianhinz <hinzfabian.fh@gmail.com>
2020-05-17 14:33:18 +0200

    save embeded project and restore saved ones

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
-function openStackblitz() {
-    StackBlitzSDK.openGithubProject("fabianhinz/learnToCode/tree/test/dummy")
+let vm = undefined
+
+window.onload = async () => {
+    const obj = JSON.parse(localStorage.getItem("sb"))
+    if (obj) {
+        vm = await StackBlitzSDK.embedProject("stackblitz-embedGithubProject", {
+            ...obj,
+            template: "create-react-app"
+        }, { width: '100%', height: '100%' })
+    } else {
+        vm = await getInitialLectureSetup()
+    }
+}
+
+async function save() {
+    try {
+        const files = await vm.getFsSnapshot()
+        const dependencies = await vm.getDependencies()
+        localStorage.setItem("sb", JSON.stringify({ files, dependencies }))
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+async function getInitialLectureSetup() {
+   return await StackBlitzSDK.embedGithubProject("stackblitz-embedGithubProject", "fabianhinz/learnToCode/tree/test/dummy", { width: '100%', height: '100%' })
 }
