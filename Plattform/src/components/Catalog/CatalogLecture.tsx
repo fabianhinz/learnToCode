efb2d2d05bb5329f98aa00d8187203b61a2176f1
Fabian Hinz <fabianhinz@users.noreply.github.com>
2020-08-15 13:00:43 +0200

    Feature/download lecture (#13)
    * update functions deps and vscode settings
    
    * download project files via github raw
    
    * cleanup node-fetch deps

--- Plattform/src/components/Catalog/CatalogLecture.tsx
+++ Plattform/src/components/Catalog/CatalogLecture.tsx
@@ -34,7 +34,8 @@ const CatalogLecture = (props: NodeContext<LectureNodeProps>) => {
     const handleSpeedDialAction = (action: SpeedDialParentAction) => {
         switch (action) {
             case 'downloadLecture': {
-                alert('todo')
+                // ! ToDo download file depending on frontmatter params
+                window.open(`https://github.com/fabianhinz/learnToCode/raw/master/dummy/dummy.zip`)
                 break
             }
             case 'openStackblitz': {
