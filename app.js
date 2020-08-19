816ee7c38feb4287f6ef09102e7fdf51c4c4cdd9
fabianhinz <hinzfabian.fh@gmail.com>
2020-08-19 17:57:50 +0200

    Root Readme

--- app.js
+++
@@ -1,5 +0,0 @@
-window.onload = getInitialLectureSetup
-
-function getInitialLectureSetup() {
-   StackBlitzSDK.embedGithubProject("stackblitz-embedGithubProject", "fabianhinz/learnToCode/tree/master/dummy", { width: '100%', height: '100%' })
-}
